03c889370894f89d046e89dc96ce7275390b649c
harshal-0101 <mahajanharshalnitin@gmail.com>
2025-01-19 18:10:33 +0530

    creating navbar

---
+++ front-end/src/Components/Nav.js
@@ -0,0 +1,70 @@
+import React,{ useState } from 'react'
+import './components.css'
+
+export default function Nav() {
+
+    const currencies = [
+        { code: "INR", name: "Indian Rupee" },
+        { code: "USD", name: "US Dollar" },
+        { code: "GBP", name: "British Pound" },
+        { code: "AUD", name: "Australian Dollar" },
+      ];
+      const [selectedCurrency, setSelectedCurrency] = useState("");
+      const handleCurrencyChange = (event) => {
+        setSelectedCurrency(event.target.value);
+      };
+
+  return (
+    <div>
+      <nav>
+        <div className='nav-container'>
+            <div className="logo">
+                MyTrip
+            </div>
+            <div className="list">
+                <ul>
+                    <li>
+                        <a href="">
+                            <p>My trip</p>
+                            <span>Manage and Booking</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                          <p>Propertiys</p>
+                          <span>List your Prackege and Propertiys</span>
+                        </a>
+                    </li>
+                    <li className='login'>
+                        <a href="">Create Account/Login</a>
+                    </li>
+                    <li>
+                      <select name="Country" id="" className='Country'>
+                        <option value="India">India | ENG</option>
+                        <option value="USA">UAS | ENG</option>
+                      </select>
+                    </li>
+                    <li>
+                      <div className="dropdown">
+                           <div className="dropdown-content">
+                             <select
+                               value={selectedCurrency}
+                               onChange={handleCurrencyChange}
+                               className="currency-select"
+                             >
+                               {currencies.map((currency) => (
+                                 <option key={currency.code} value={currency.code}>
+                                   {currency.code} 
+                                 </option>
+                               ))}
+                             </select>
+                           </div>
+                      </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
